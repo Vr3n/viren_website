4bcaab70b78f6462b17b7e833f6fe3eca19ed531
vr3n <vickyspatel@gmail.com>
2022-03-24 20:13:59 +0530

    About section change

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,9 @@ const Home = () => {
         <Hero />
         <BgAnimation />
       </Section>
+      <Timeline />
       <Projects />
       <Technologies />
-      <Timeline />
       {/* <Acomplishments /> */}
     </Layout>
   );
